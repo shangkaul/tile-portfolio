fa8040c1a0be26442f765ef36dff2d15893a480d
imakish <makishtp@gmail.com>
2021-10-12 21:10:22 +0530

    added backdrop and navbar closes on outside click

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -19,42 +19,50 @@ export default function Nav(props) {
             <FaIcons.FaBars onClick={showSidebar} style={barStyle} />
           </Link>
         </div>
-        <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
-          <ul className="nav-menu-items" onClick={showSidebar}>
-            <li className="navbar-toggle">
-              <Link to="#" className="menu-close">
-                <AiIcons.AiOutlineClose style={closeStyle} />
-              </Link>
-            </li>
-            <li className="nav-name">
-              <Link to="/">John Doe</Link>
-            </li>
-            {NavbarData.map((item, index) => {
-              return (
-                <li key={index} className={item.cName}>
-                  <Link to={item.path}>
-                    {item.icon}
-                    <span>{item.title}</span>
-                  </Link>
-                </li>
-              );
-            })}
-          </ul>
-          <div className="footer">
-            <a style={{ paddingLeft: "1.5rem" }} href="/">
-              <FaIcons.FaLinkedin />
-            </a>
-            <a href="/">
-              <FaIcons.FaInstagram />
-            </a>
-            <a href="/">
-              <FaIcons.FaTwitter />
-            </a>
-            <a href="/">
-              <FaIcons.FaGithub />
-            </a>
-          </div>
-        </nav>
+        <div
+          className={sidebar ? "backdrop active" : "backdrop"}
+          onClick={showSidebar}
+        >
+          <nav
+            className={sidebar ? "nav-menu active" : "nav-menu"}
+            onClick={(e) => e.stopPropagation()} // Prevent click from closing card
+          >
+            <ul className="nav-menu-items" onClick={showSidebar}>
+              <li className="navbar-toggle">
+                <Link to="#" className="menu-close">
+                  <AiIcons.AiOutlineClose style={closeStyle} />
+                </Link>
+              </li>
+              <li className="nav-name">
+                <Link to="/">John Doe</Link>
+              </li>
+              {NavbarData.map((item, index) => {
+                return (
+                  <li key={index} className={item.cName}>
+                    <Link to={item.path}>
+                      {item.icon}
+                      <span>{item.title}</span>
+                    </Link>
+                  </li>
+                );
+              })}
+            </ul>
+            <div className="footer">
+              <a style={{ paddingLeft: "1.5rem" }} href="/">
+                <FaIcons.FaLinkedin />
+              </a>
+              <a href="/">
+                <FaIcons.FaInstagram />
+              </a>
+              <a href="/">
+                <FaIcons.FaTwitter />
+              </a>
+              <a href="/">
+                <FaIcons.FaGithub />
+              </a>
+            </div>
+          </nav>
+        </div>
       </IconContext.Provider>
     </>
   );
