f43b9d6b021ee5ac215f5cf4133dbc3d2fbf6edb
shangkaul <shangkaul@gmail.com>
2021-10-24 13:10:12 +0530

    fix mobile view

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,5 +1,5 @@
 import "./Nav.css";
-import {FaLinkedin, FaTwitter, FaInstagram, FaGithub} from "react-icons/fa";
+import {FaLinkedin, FaEnvelope, FaInstagram, FaGithub} from "react-icons/fa";
 import {AiOutlineMenu} from "react-icons/ai";
 import { Link } from "react-router-dom";
 
@@ -8,23 +8,23 @@ export default function Nav(props)
    return(
        <div className="Nav">
            <div className="logo">
-           <li><Link to='/' className="link"> John </Link></li>
-           <li><Link to='/' className="link"> Doe </Link></li>
+           <li><Link to='/' className="link"> Shivang </Link></li>
+           <li><Link to='/' className="link"> Kaul </Link></li>
            </div>
            <li><Link to='/' className="link"> Home </Link></li>
            <li><Link to='/about' className="link">About </Link></li>
-           <li><Link to='/about' className="link">Projects </Link></li>
            <li><Link to='/about' className="link">Work </Link></li>
+           <li><Link to='/about' className="link">Projects </Link></li>
+           <li><Link to='/about' className="link">Resume </Link></li>
            <li><Link to='/about' className="link">Services </Link></li>
-           <li><Link to='/about' className="link">Testimonials </Link></li>
-           <li><Link to='/about' className="link">Clients </Link></li>
+           <li><Link to='/about' className="link">Blog </Link></li>
            <li><Link to='/about' className="link">Contact </Link></li>
 
            <div className="footer">
-               <a style={{paddingLeft:"1.5rem"}} href="/"><FaLinkedin/></a>
-               <a href="/"><FaInstagram/></a>
-               <a href="/"><FaTwitter/></a>
-               <a href="/"><FaGithub/></a>
+               <a style={{paddingLeft:"1.5rem"}} href="https://www.linkedin.com/in/shivangkaul/"><FaLinkedin/></a>
+               <a href="https://www.instagram.com/shangkaul/"><FaInstagram/></a>
+               <a href="mailto:shangkaul@gmail.com"><FaEnvelope/></a>
+               <a href="https://github.com/shangkaul"><FaGithub/></a>
 
            </div>
            <div className="navBtn"><AiOutlineMenu/></div>
